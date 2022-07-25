a3ac3557a137303aee1624fa13d94e106bf910ed
Wagner Parisoto <wagnerparisoto@MacBook-Air-de-Wagner.local>
2022-07-24 21:46:04 -0300

    code

--- movies_lib/src/components/Navbar.jsx
+++ movies_lib/src/components/Navbar.jsx
@@ -1,26 +1,44 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi"
 
 import "./NavBar.css"
+import { useState } from "react"
 
 const Navbar = () => {
-    return (
-        <nav id="navbar">
-        <h2>
-          <Link to="/">
-            <BiCameraMovie/> MoviesLib
-          </Link>
-        </h2>
 
-        <form>
-            <input type="text" placeholder="Busque um filme"></input>
-            <button type="submit">
-                <BiSearchAlt2/>
-            </button>
-        </form>
-      </nav>
+  const [search, setSearch] = useState("")
+  const navigate = useNavigate()
 
-    )
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(search)
+
+    if(!search) return
+
+    navigate(`/search?q=${search}`)
+    setSearch('')
+  }
+
+  return (
+    <nav id="navbar">
+      <h2>
+        <Link to="/">
+          <BiCameraMovie /> MoviesLib
+        </Link>
+      </h2>
+
+      <form onSubmit={handleSubmit}>
+        <input type="text" 
+          placeholder="Busque um filme" 
+          onChange={(e) => setSearch(e.target.value)}
+          value={search}></input>
+        <button type="submit">
+          <BiSearchAlt2 />
+        </button>
+      </form>
+    </nav>
+
+  )
 }
 
 export default Navbar
--- movies_lib/src/pages/Search.jsx
+++ movies_lib/src/pages/Search.jsx
@@ -1,6 +1,39 @@
+
+const searchUrl = import.meta.env.VITE_SEARCH
+const apiKey = import.meta.env.VITE_API_KEY
+
+import { useEffect, useState } from "react"
+import { useSearchParams } from "react-router-dom"
+import MovieCard from "../components/MovieCard"
+import "./MoviesGrid.css"
+
 const Search = () => {
+    const [searchParam] = useSearchParams()
+
+    const [movies, setMovies] = useState([])
+    const query = searchParam.get("q")
+
+    const getSearchedMovies = async (url) =>{
+        const res = await fetch(url);
+        const data = await res.json();
+        setMovies(data.results)
+    };
+
+    useEffect(() =>{
+        const searchWithQueryURL = `${searchUrl}?${apiKey}&query=${query}`
+        getSearchedMovies(searchWithQueryURL)
+    }, [query])
+
     return (
-        <div>Search</div>
+        <div className="container">
+            <h2 className="title">
+                Resultados para: <span className="query-text">{query}</span>
+            </h2>
+            <div className="movies-container">
+                {movies.length === 0 && <p>Carregando...</p>}
+                {movies.length > 0 && movies.map((movie) => <MovieCard key={movie.id} movie={movie}></MovieCard> )}
+            </div>
+        </div>
     )
 }
 
